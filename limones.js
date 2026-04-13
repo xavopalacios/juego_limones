aeb3b5fa9b9ba910ac59761b9fdf8f615b5764bf
xavopalacios <xavopalacios@gmail.com>
2026-04-13 18:58:21 -0500

    The functions iniciarJuego, dibujarSuelo, dibujarPersonaje were created, and the constants ALTURA_PISO, ALTO_PERSONAJE and ANCHO_PERSONAJE were created

--- limones.js
+++ limones.js
@@ -1,2 +1,27 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+let puntaje=0;
+let personajeX=0;
+let personajeY=0;
+let limonX=0;
+let limonY=0;
+
+const ALTURA_SUELO=20;
+const AlTO_PERSONAJE=40;
+const ANCHO_PERSONAJE=20;
+
+function iniciarJuego(){    
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function dibujarSuelo(){
+    ctx.fillStyle="#27b83f";
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO)
+    
+}    
+
+function dibujarPersonaje(){
+    ctx.fillStyle= "yellow";
+    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+AlTO_PERSONAJE),ANCHO_PERSONAJE,AlTO_PERSONAJE);
+}
