332fcd7415baadd1818f2221d657a37f164bdd68
xavopalacios <xavopalacios@gmail.com>
2026-04-14 09:46:26 -0500

    feat: se The vercel.json file was added

--- limones.js
+++ limones.js
@@ -86,7 +86,7 @@ function detectarAtrapado(){
 }
 
 function generarAleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min +1 )) + min; // con esta función se genera un número aleatorio entre el mínimo y máximo 
+    return Math.floor(Math.random() * (max - min +1 )) + min; // con esta función se genera un número aleatorio incluidos el mínimo y máximo 
 }
 
 function detectarPiso(){
