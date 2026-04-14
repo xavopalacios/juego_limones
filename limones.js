5b9349de881b32185e95ae61e326447792d8a3c4
xavopalacios <xavopalacios@gmail.com>
2026-04-13 23:42:31 -0500

    feat:The functions dibujarLimon and bajarLimon were created

--- limones.js
+++ limones.js
@@ -3,16 +3,19 @@ let ctx=canvas.getContext("2d");
 let puntaje=0;
 let personajeX=canvas.width/2;
 let personajeY=0;
-let limonX=0;
-let limonY=0;
+let limonX=canvas.width/2;
+let limonY=5;
 
 const ALTURA_SUELO=20;
 const ALTO_PERSONAJE=40;
 const ANCHO_PERSONAJE=20;
+const ALTO_LIMON=20;
+const ANCHO_LIMON=20;
 
 function iniciarJuego(){    
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 
 function dibujarSuelo(){
@@ -25,6 +28,10 @@ function dibujarPersonaje(){
     ctx.fillStyle= "yellow";
     ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTO_PERSONAJE),ANCHO_PERSONAJE,ALTO_PERSONAJE);
 }
+function dibujarLimon(){
+    ctx.fillStyle= "green";
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTO_LIMON);
+}
 
 function moverIzquierda(){
     personajeX=personajeX-10;
@@ -36,12 +43,19 @@ function moverDerecha(){
     actualizarPantalla();
 }
 
+function bajarLimon(){
+    limonY=limonY+10;
+    actualizarPantalla();
+}
+
 function actualizarPantalla(){
     limpiarCanvas();
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 
 function limpiarCanvas(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
 }
+
